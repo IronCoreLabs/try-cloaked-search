b2bae2d5ff248a627ad7e01ec5c08d266a43561d
Patrick Walsh <patrick.walsh@ironcorelabs.com>
2021-07-15 13:39:40 -0600

    fixes for mac

--- populate_index.sh
+++ populate_index.sh
@@ -3,8 +3,11 @@ xargs_c='unknown'
 unamestr=`uname`
 if [[ "$unamestr" == 'Linux' ]]; then
    xargs_c='xargs'
-elif [[ "$unamestr" == 'FreeBSD' ]]; then
+elif [[ "$unamestr" == 'FreeBSD' || "$unamestr" == 'Darwin' ]]; then
    xargs_c='xargs -S 20000'
+else
+   echo "Unknown platform"
+   exit 2
 fi
 # Create the index in elastic search
 curl -s -X PUT localhost:8675/try_cloaked_search/
@@ -16,4 +19,4 @@ NUM_THREADS=5
 # Then call curl to index for up to NUM_THREADS at a time
 echo ""
 echo "Num indexed: "
-tr \\n \\0 < wikipedia-articles-1000-1.json | "$xargs_c" -0 -n 1 -P$NUM_THREADS -I '{}' curl -s -X POST -H Content-Type:application/json http://localhost:8675/try_cloaked_search/_doc -d '{}' | grep -o seq_no | wc -l
+tr \\n \\0 < wikipedia-articles-1000-1.json | $xargs_c -0 -n 1 -P$NUM_THREADS -I '{}' curl -s -X POST -H Content-Type:application/json http://localhost:8675/try_cloaked_search/_doc -d '{}' | grep -o seq_no | wc -l

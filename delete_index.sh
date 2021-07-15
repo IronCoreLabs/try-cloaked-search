a1666387e42e0668ef6270eb95257229676a4882
Clint Frederickson <clint.frederickson@ironcorelabs.com>
2021-07-15 08:07:09 -0600

    only include the populate_index and data

--- delete_index.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Delete the test index in elastic search
-curl -s -X DELETE "localhost:8675/try_cloaked-search/?pretty"
-
--- populate_index.sh
+++ populate_index.sh
@@ -1,15 +1,19 @@
-#!/bin/sh
-
+#!/bin/bash
+xargs_c='unknown'
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+   xargs_c='xargs'
+elif [[ "$unamestr" == 'FreeBSD' ]]; then
+   xargs_c='xargs -S 20000'
+fi
 # Create the index in elastic search
-curl -s -X PUT localhost:8675/try_cloaked-search/
+curl -s -X PUT localhost:8675/try_cloaked_search/
 
 # Configure the parallelism of the index
-NUM_THREADS=10
+NUM_THREADS=5
 
 # Break the input up into individual wikipedia articles
 # Then call curl to index for up to NUM_THREADS at a time
+echo ""
 echo "Num indexed: "
-tr "\n" "\0" < ./wikipedia-articles-1000-1.json | \
-  xargs -0 -S 20000 -E "\n" -n 1 -P$NUM_THREADS -I '{}' \
-  curl -s -X POST -H Content-Type:application/json http://localhost:8675/try_cloaked-search/_doc -d '{}' | \
-  grep -o seq_no | wc -l
+tr \\n \\0 < wikipedia-articles-1000-1.json | "$xargs_c" -0 -n 1 -P$NUM_THREADS -I '{}' curl -s -X POST -H Content-Type:application/json http://localhost:8675/try_cloaked_search/_doc -d '{}' | grep -o seq_no | wc -l

9de6154ca295f7d0d6d75af971b4e95f1a94394e
Murph Murphy <murph@clurictec.com>
2024-02-22 17:00:37 -0700

    Update k8s example (#31)
    * First pass at updated k8s example
    
    * Everything tested and working

--- query-cloaked-search.sh
+++ query-cloaked-search.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 function usage () {
   echo "Makes queries to a local version of the cloaked search proxy."
--- query-search-service.sh
+++ query-search-service.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 function usage () {
   echo "Makes queries to a local version of the search service."
